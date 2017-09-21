4ddc87ea2011d691156abf80d6e9a5031017f93a
Stefan Milic <stefan.milic@diwanee.com>
2017-09-21 12:24:53 +0200

    homepage completed

---
+++ app/js-dev/sliders/business-slider.js
@@ -0,0 +1,29 @@
+(function($){
+
+$(window).on('load', function() {
+	//caching elements
+	var $root = $('.business'),
+		$carousel = $root.find('.slider'),
+		$slides = $carousel.find('.slide'),
+		$nextArrow = $root.find('.slider__next'),
+		$prevArrow = $root.find('.slider__prev'),
+		$current = $root.find('.current'),
+		$total = $root.find('.total');
+
+	$total.text('0'+$slides.length);//total number of slides into html as txt
+
+	$carousel.slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		nextArrow:$nextArrow,
+		prevArrow:$prevArrow,
+	});
+
+	$carousel.on('afterChange', function(event, slick, currentSlide){
+		
+		$current.text('0'+(currentSlide+1));
+	});
+		
+});
+
+})(jQuery);
