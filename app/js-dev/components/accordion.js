417e93e7779371e40b55b01cc0b238372bf62fbb
Stefan Milic <stefan.milic@diwanee.com>
2017-09-29 16:33:46 +0200

    'Article pages completed 70%'

---
+++ app/js-dev/components/accordion.js
@@ -0,0 +1,42 @@
+(function($){
+	//variables
+	var $root = $('.accordion'),
+		$tab = $root.find('.accordion-tab'),
+		$title = $root.find('.accordion-title'),
+		$body = $root.find('.accordion-body'),
+		$btn = $root.find('.accordion-btn'),
+		$icon = $btn.find('i');
+		console.log($tab);
+		
+		$tab.click(function(){
+			//console.log($(this));
+			//$('.accordion-body.active').slideUp(300).removeClass('active');
+			var $clickedItem = $(this).find('.accordion-body');
+			var $itemActive = $root.find('.accordion-body.active');
+			var $clickedBtn = $(this).find('.accordion-btn i');
+			var $clickedBtnActv = $(this).find('.accordion-btn i.active');
+
+			//arrows
+			if($clickedBtn.hasClass('active')){
+				$clickedBtnActv.removeClass('active').removeClass('fa-caret-up').addClass('fa-caret-down');
+				//console.log('ja sam aktivan');
+			}else{
+				//clickedBtnActv.removeClass('active').removeClass('fa-caret-up').addClass('fa-caret-down');
+				$(this).find('.accordion-btn i.active').removeClass('active').removeClass('fa-caret-up').addClass('fa-caret-down');
+				$clickedBtn.addClass('active').addClass('fa-caret-up').removeClass('fa-caret-down');
+				//console.log('ja sam NISAM aktivan');
+			}
+
+
+			//tabs
+			if($clickedItem.hasClass('active')){
+
+				$clickedItem.removeClass('active').slideUp(300);
+			}else{
+				
+				$itemActive.toggleClass('active').slideToggle(300);
+				$clickedItem.addClass('active').slideToggle(300);
+			}
+		});
+
+})(jQuery);
